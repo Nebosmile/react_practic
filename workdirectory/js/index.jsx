39dad4cf034cf122864cc126a5d90c2d21c2cc63
Pavel <akulov_pavel88@mail.ru>
2017-03-28 17:58:19 +0300

    clear console

--- workdirectory/js/index.jsx
+++ workdirectory/js/index.jsx
@@ -16,23 +16,18 @@ ReactDOM.render(
 
 store.subscribe(()=>console.log('New state', store.getState()));
 
-console.log(1);
 store.dispatch({
     type: 'INCREASE_COUNTER',
 });
-console.log(2);
 store.dispatch({
     type: 'INCREASE_COUNTER',
 });
-console.log(3);
 store.dispatch({
     type: 'RESET_COUNTER',
 });
-console.log(4);
 store.dispatch({
     type: 'Unknow',
 });
-console.log(5);
 store.dispatch({
     type: 'LOAD_ISSUES',
     payload:[{id:1,name:'First issue'},{id:2, name:"Second issue"}],
